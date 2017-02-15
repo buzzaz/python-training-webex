c83d74e9807bd15863586f7a6a7d5ea9c7089662
Abbas Zaidi <Abbaszaidi@Abbass-MacBook-Air-2.local>
2017-02-14 18:07:26 -0800

    Add examples from the past few weeks.

---
+++ week7.py
@@ -0,0 +1,14 @@
+'''
+    This program outputs its name, some of its own functions,
+    and also accepts input
+'''
+
+print ('My name is ' + __name__)
+
+__author__ - 'AZ'
+__license__ = 'GPLv3'
+
+if __license__ == 'GPLv3':
+    print ('Corporations Do Not Like This')
+
+snack_fruits = ['pear', 'banana', 'apricots', 'apples']
